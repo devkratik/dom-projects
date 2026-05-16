93c7a1699c1ae5b0e5cb8cceec3b318fec90637c
Kratik777 <kratikbhatt6@gmail.com>
2026-05-16 14:50:15 +0530

    Password Generator app completed

--- 010-Password Generator/script.js
+++ 010-Password Generator/script.js
@@ -1,2 +1,151 @@
 "use strict";
-console.log("hi");
+
+// DOM Elements - all the elements we need from HTML
+const passwordInput = document.getElementById("password");
+const lengthSlider = document.getElementById("length");
+const lengthDisplay = document.getElementById("length-value");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const lowercaseCheckbox = document.getElementById("lowercase");
+const numbersCheckbox = document.getElementById("numbers");
+const symbolsCheckbox = document.getElementById("symbols");
+const generateButton = document.getElementById("generate-btn");
+const copyButton = document.getElementById("copy-btn");
+const strengthBar = document.querySelector(".strength__bar");
+const strengthText = document.querySelector(".strength__container p");
+const strengthLabel = document.getElementById("strength-label");
+
+// Character sets
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numberCharacters = "0123456789";
+const symbolCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?/";
+
+lengthSlider.addEventListener("input", () => {
+  lengthDisplay.textContent = lengthSlider.value;
+});
+
+generateButton.addEventListener("click", makePassword);
+
+function makePassword() {
+  const length = Number(lengthSlider.value);
+  const includeUppercase = uppercaseCheckbox.checked;
+  const includeLowercase = lowercaseCheckbox.checked;
+  const includeNumbers = numbersCheckbox.checked;
+  const includeSymbols = symbolsCheckbox.checked;
+
+  if (
+    !includeUppercase &&
+    !includeLowercase &&
+    !includeSymbols &&
+    !includeNumbers
+  ) {
+    alert(`Please select atleast one char type.`);
+    return;
+  }
+
+  const newPassword = getPassword(
+    length,
+    includeLowercase,
+    includeNumbers,
+    includeSymbols,
+    includeUppercase,
+  );
+
+  passwordInput.value = newPassword;
+
+  updateStrengthMeter(newPassword);
+}
+
+function getPassword(
+  length,
+  includeLowercase,
+  includeNumbers,
+  includeSymbols,
+  includeUppercase,
+) {
+  let allCharacters = "";
+  //ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?/</>
+
+  if (includeUppercase) allCharacters += uppercaseLetters;
+  if (includeLowercase) allCharacters += lowercaseLetters;
+  if (includeNumbers) allCharacters += numberCharacters;
+  if (includeSymbols) allCharacters += symbolCharacters;
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters[randomIndex];
+  }
+
+  return password;
+}
+
+function updateStrengthMeter(password) {
+  const passwordLength = password.length;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /[0-9]/.test(password);
+  const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  let strengthScore = 0;
+  //this will make sure that we always get maximum as 40 no matter password length is 24 resulting first parameter to be 24*2=48
+  strengthScore += Math.min(passwordLength * 2, 40);
+
+  if (hasUpperCase) strengthScore += 15;
+  if (hasLowerCase) strengthScore += 15;
+  if (hasNumbers) strengthScore += 15;
+  if (hasSymbols) strengthScore += 15;
+
+  // enforce every short password a minimum score of 40
+  if (passwordLength < 8) strengthScore = Math.min(strengthScore, 40);
+
+  // ensure that the width of strength is valid percentage
+  const safeScore = Math.max(5, Math.min(100, strengthScore));
+  strengthBar.style.width = safeScore + "%";
+
+  let strengthLabelText = "";
+  let barColor = "";
+
+  if (strengthScore < 40) {
+    //weak password:
+    barColor = `var(--color-weak)`;
+    strengthLabelText = "Weak";
+  } else if (strengthScore < 70) {
+    //medium password:
+    barColor = `var(--color-medium)`;
+    strengthLabelText = "Medium";
+  } else {
+    //strong password:
+    barColor = `var(--color-strong)`;
+    strengthLabelText = "Strong";
+  }
+
+  strengthBar.style.backgroundColor = barColor;
+  strengthLabel.textContent = strengthLabelText;
+}
+window.addEventListener("DOMContentLoaded", makePassword);
+
+copyButton.addEventListener("click", () => {
+  if (!passwordInput.value) {
+    return;
+  }
+  navigator.clipboard
+    .writeText(passwordInput.value)
+    .then(() => showCopySuccess())
+    .catch((err) => {
+      console.log(`Could not copy: ${err}`);
+    });
+});
+
+function showCopySuccess() {
+  copyButton.classList.remove("far", "fa-copy");
+  copyButton.classList.add("fas", "fa-check");
+  copyButton.style.olor = `var(--color-strong)`;
+
+  setTimeout(() => {
+    copyButton.classList.remove("fas", "fa-check");
+    copyButton.classList.add("far", "fa-copy");
+    copyButton.style.color = "";
+  }, 1500);
+}

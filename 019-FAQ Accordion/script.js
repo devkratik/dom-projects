09f4d5b311f5d9098d5f9c4eada2cf6cb654b1fd
Kratik777 <kratikbhatt6@gmail.com>
2026-08-16 16:37:37 +0530

    Update

--- 019-FAQ Accordion/script.js
+++ 019-FAQ Accordion/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const faqItemContainerEl = document.querySelector(".faq-list");
+
+faqItemContainerEl.addEventListener("click", handleActiveQuestion);
+
+function handleActiveQuestion(e) {
+  const clickedItem = e.target.closest(".faq-item");
+
+  if (!clickedItem) return;
+
+  const wasActive = clickedItem.classList.contains("active");
+
+  const allQuestions = document.querySelectorAll(".faq-item");
+
+  allQuestions.forEach((question) => {
+    question.classList.remove("active");
+  });
+
+  // Only open it if it wasn't already open
+  if (!wasActive) {
+    clickedItem.classList.add("active");
+  }
+}
